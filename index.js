30274733d3c08bf529aff7ac6472c10f756a60ed
Sarvarbek-cmd <97881825+Sarvarbek-cmd@users.noreply.github.com>
2022-12-10 13:34:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+TELEGRAM_BOT_TOKEN = '5287754572:AAHndHr6IdgK9hKALXs0ADcpwsDCLEwgK70'
+const { randomBytes } = require('crypto');
+const TeleBot = require('telebot');
+const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
+const chatIds = ['https://t.me/SarvarDev0312','https://t.me/QodirovDev','https://t.me/shohruz_coder']
+
+bot.on('text', (msg) => msg.reply.text('iltimos shu lichga yozing' + chatIds));
+bot.inlineKeyboard(GamepadButton)
+bot.start();
